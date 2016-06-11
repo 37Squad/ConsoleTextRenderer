bb3692d3861b24a344fed6fef1ec57580aa14601
Gorshborsh <gorshborsh51@gmail.com>
2016-06-11 16:38:58 -0500

    More work on Glyphs!

--- ConsoleTextRenderer/ConsoleTextRenderer/GlyphManager.cs
+++ ConsoleTextRenderer/ConsoleTextRenderer/GlyphManager.cs
@@ -8,28 +8,80 @@ namespace ConsoleTextRenderer
 {
     class GlyphManager
     {
-        //holds file path for the glyph-containing texture
-        private String filePath = String.Empty;
+        //'glyphPos' and 'glyphLine' are indexes for the 'glyphs' array
         //holds current glpyh position in the current line
-        private int glphyPos = 0;
+        private int glyphPos = 0;
         //holds current glyph line
         private int glyphLine = 0;
         //this is the array that contains the glpyh data
-        private char[][] glpyhs = null;
+        private Glyph[][] glyphs = null;
+        //max lines
+        private int maxLines = 0;
+        //max characters per line
+        private int maxCharacters = 0;
 
-        public GlyphManager(String glyphFilePath,int lines,int characters)
+        //Constructor
+        public GlyphManager(int lines,int characters)
         {
-            this.filePath = glyphFilePath;
+            this.maxCharacters  = characters;
+            this.maxLines       = lines;
+            this.ClearGlpyhs();
         }
 
-        public String GetFilePath()
+        //Return maximum lines
+        public int GetMaxLines()
         {
-            return this.filePath;
+            return this.maxLines;
         }
 
+        //Return maximum characters per line
+        public int GetMaxCharacters()
+        {
+            return this.maxCharacters;
+        }
+
+        //Reset all glyphs
         public void ClearGlpyhs()
         {
+            //Iterate over 'glyphs'
+            for(int x = 0; x < this.maxLines;x++)
+            {
+                for(int y = 0; y < this.maxCharacters;y++)
+                {
+                    //this is the NULL character - we will use it to indicate that there is no glyph stored here
+                    this.glyphs[x][y] = Glyph.GLYPH_EMPTY;
+                }
+            }
 
+            //Reset indexes too!
+            this.glyphLine  = 0;
+            this.glyphPos   = 0;
+        }
+
+        //Write a glyph
+        public void WriteChar(char character)
+        {
+            this.glyphs[glyphLine][glyphPos++] = new Glyph(character);
+            if(glyphPos >= this.maxCharacters)
+            {
+                glyphLine++;
+            }
+            if(glyphLine >= this.maxLines)
+            {
+                //What do we do with an overflow? I don't know!
+                //Clear it!
+                //For now...
+                this.ClearGlpyhs();
+            }
+        }
+
+        //Basically a wrapper for WriteChar
+        public void WriteString(String characters)
+        {
+            foreach(char character in characters)
+            {
+                this.WriteChar(character);
+            }
         }
 
     }
